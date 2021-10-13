94148105a6774f4bef1fe0d53b7da5cb5a78dc85
JaskiratAnand <70444793+JaskiratAnand@users.noreply.github.com>
2021-10-14 02:03:28 +0530

    task 2 done

--- submissions/2002463_task2.cpp
+++ submissions/2002463_task2.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main() {
-    //defining datatypes of variables
     int a;
     char ch;
     double d;
     short s;
     long l;
 
-    //using sizeof() to get memory size of each data type & using cout to print it.
     cout << "\nSize of int: " << sizeof(a);
     cout << "\nSize of char: " << sizeof(ch);
     cout << "\nSize of double: " << sizeof(d);
