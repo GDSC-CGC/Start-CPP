b0037783189d861516c3e5b07f446656e331a476
harshilsharmaa <71216106+harshilsharmaa@users.noreply.github.com>
2021-10-13 05:27:50 -0800

    Add files via upload

---
+++ submissions/2002848_task4_.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int sumUptoN(int n){
+    int sum = 0;
+    for(int i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    cout<<sumUptoN(n)<<endl;
+    
+    return 0;
+}
