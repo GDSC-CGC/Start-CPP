10b4f4a8e7861e73b259fd4c50de55dc9def43f2
Hermehar <63737394+Hermehar@users.noreply.github.com>
2021-10-13 12:08:50 +0530

    Create 2002453_task-2.cpp

---
+++ submissions/2002453_task-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Size of int data type is: "<<sizeof(int)<<endl;
+    cout<<"Size of float data type is: "<<sizeof(float)<<endl;
+    cout<<"Size of boolean data type is: "<<sizeof(bool)<<endl;
+    cout<<"Size of character data type is: "<<sizeof(char)<<endl;
+    cout<<"Size of double floating data type is: "<<sizeof(double)<<endl;
+    
+    return 0;
+}
