2a289468007c8b8641008250011cf49ac1d19301
snehapatil455 <snehapatil6477@gmail.com>
2021-10-24 18:41:46 +0530

    Added task2

---
+++ submissions/TCOB99_task-2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Size of boolean data type is: "<<sizeof(bool)<<" byte\n";
+    cout<<"Size of char data type is : "<<sizeof(char)<<" byte\n";
+    cout<<"Size of int data type is : "<<sizeof(int)<<" byte\n";
+    cout<<"Size of floating point data type is: "<<sizeof(float)<<" byte\n";
+    cout<<"Size of double floating point data type is : "<<sizeof(double)<<" byte\n";
+    return 0;
+}
+
